4e20057b4a248def36d811e123289893020f92d4
Diego Leite <di3go@Tesla.home>
2015-05-28 17:54:22 -0300

    uhuuuuul 1371 done

--- contests/diego/26-05-2015/1371.cpp
+++ contests/diego/26-05-2015/1371.cpp
@@ -1,32 +1,22 @@
 #include <cstdio>
-#include <cstring>
-#include <cmath>
 
 int main() {
-	int n;
+	int n, temp;
 	
 	while(scanf("%d", &n) && n) {
-		bool doors[n];
-		int middle = floor(n/2);
-
-		memset(doors, 1, sizeof(bool)*n);
 		
-		for(int i=2; i<=n; i++) {
-			if(i<=middle) {
-				for(int j=1; j<=n; j++) {
-					if(j%i == 0)
-						doors[j-1] = !doors[j-1];
-				}
+		for(int i=1; ; i++) {
+			temp = i*i;
+
+			if(temp <= n) {
+				if((i+1)*(i+1) <= n)
+					printf("%d ", temp);
+				else
+					printf("%d", temp);
 			} else {
-				doors[i-1] = !doors[i-1];
+				break;
 			}
 		}
-		
-		for(int i=0; i<n; i++) {
-			if(doors[i] == 1)
-				printf("%d ", i+1);
-		}
-
 		printf("\n");
 	}
 	return 0;
